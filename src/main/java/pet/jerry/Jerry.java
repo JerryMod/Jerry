1d579ed3f5786903484d7e13dea346896ed188bf
eris <eris@newdreams.ltd>
2021-01-25 16:50:41 -0500

    build updates that hopefully work

--- src/main/java/pet/jerry/Jerry.java
+++ src/main/java/pet/jerry/Jerry.java
@@ -43,7 +43,7 @@ import java.util.UUID;
 public class Jerry {
 	public static final String MOD_ID = "jerry";
 	public static final String MOD_NAME = "Jerry";
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "@VERSION@";
 
 	@Mod.Instance(MOD_ID)
 	public static Jerry INSTANCE;
